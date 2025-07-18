b1a3565f51574e6863dbf613f20ead1f5a408770
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2025-07-18 13:28:21 +0100

    Comment out console prints.

--- src/utils/objParser.ts
+++ src/utils/objParser.ts
@@ -5,16 +5,16 @@ import ObjFileParser from 'obj-file-parser'
  * of interleaved position and color values.
  */
 export function parseObjToVertices(objText: string): Float32Array {
-  console.log("objText", objText)
+//   console.log("objText", objText)
   const parser = new ObjFileParser(objText)
   const parsed = parser.parse()
   const vertices: number[] = []
   const { vertices: v, faces } = parsed.models[0]
 
   for (const face of faces) {
-    console.log("face", face)
+    // console.log("face", face)
     for (const vertex of face.vertices) {
-      console.log("v[vertex.vertexIndex]", v[vertex.vertexIndex - 1])
+    //   console.log("v[vertex.vertexIndex]", v[vertex.vertexIndex - 1])
       const pos = v[vertex.vertexIndex - 1]
       vertices.push(pos.x, pos.y, pos.z)
       vertices.push(1.0, 1.0, 1.0) // default white color
