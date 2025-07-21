a042dcf12d2fae8dccb8d5e19067f226853a3de9
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2025-07-21 11:09:53 +0100

    Fix canvas expansion issue by constraining size with scoped CSS in PreviewPanel.vue.

--- src/components/PreviewPanel.vue
+++ src/components/PreviewPanel.vue
@@ -20,5 +20,13 @@ defineExpose({
   display: flex;
   flex-direction: column;
   height: 100%;
+  overflow: hidden;
+}
+
+canvas {
+  display: block;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
 }
 </style>
