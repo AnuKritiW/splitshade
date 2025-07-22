431aa68dce8ab84b6836a9c39af17787cc3f31fe
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2025-07-22 16:11:40 +0100

    Configure Vitest and alias support for unit testing.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,14 @@ export default defineConfig({
     outDir: 'dist',
   },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src') // maps @/ to /src
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    // setupFiles: './test/setup.ts'
+  }
 })
